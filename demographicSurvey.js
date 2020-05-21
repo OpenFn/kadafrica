36361d6fd3ea71415cea10ba7a760b01e20815d1
aleksa-krolls <aleksagk@gmail.com>
2020-05-21 00:22:33 -0700

    survey demo updates

--- demographicSurvey.js
+++ demographicSurvey.js
@@ -1,35 +1,38 @@
 //Job to create a Survey in DB for every Kobo submission
 sql(state => {
   const { data } = state;
+  const statusMap = { 'single' : 1, 'divorcee' : 2, 'In a monogamous relationship/marriage' : 3 };
+  const ageValid = ( state.data.form.Age > 15 ? "Passed" : "Check age and DOB");
+
   return (
     `insert into "girls" ("` +
     [
       'survey_id',
       'submission_time',
       'girl_id',
-      'kadafrica_site', 
-      'first_name', 
+      'kadafrica_site',
+      'first_name',
       'surname',
       'phone',
       'marital_status',
-      'age_married',
+      'age',
       'data_check',
-      'reasons_joining'
+      'main_joining_reason'
     ].join('", "') +
     `") values ('` +
     [
-      data.data._uuid,
-      data.data._submission_time,
-      data.data['Girl ID'],
-      data.data['KadAfrica Site'], 
-      data.data.first_name,
-      data.data.surname,
-      data.data.phone_number, //append 0 to front, if not null
-      data.data.marital_status, // return statusMAP ==='Single' : 1, 'Divorcee' : 2, 'Monogamous Relationship' : 3
-      data.data.age_married,
-      data.data.data_check, // (age_married < 11 : "Confirm age married" ? null)
-      data.data.reasons_joining /* return concatenated string only IF if these individual values ===1 : '${`reasons_income`}, ${`save_money`}, ${`reduce_poverty`}' . If 0, do not include in string*/
-      
+      data.form._uuid,
+      data.form._submission_time,
+      data.form.Girl_ID,
+      data.form.KadAfrica_Site,
+      data.form.First_Name,
+      data.form.Surname,
+      `0${data.form.Your_Phone_Number}`,   // append 0 to phone number
+      statusMap[data.form.What_is_your_marital_status], //map marital_status to coded responses to return different value, see Line 4
+      data.form.Age,
+      ageValid, // validate that age is over 15 yrs, flag that data check needed if < 15yrs, see line 5
+      data.form.What_is_your_main_reason_for_j
+
     ]
       .join("', '")
       .replace(/''/g, null) +
